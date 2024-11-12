91ead40cac7ca00d3721145032d866954263457a
Juris Būmanis <juris.bumanis@pata.lv>
2024-11-12 16:55:12 +0200

    main: cleanup

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\Dashboard\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [AuthController::class, 'view'])->name('home');
